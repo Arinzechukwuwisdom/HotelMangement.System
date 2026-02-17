979190429d82c15b6c07c3c056ce3b726b847bfe
Arinzechukwu Ogbu <arinzechukwuwisdomogbu@gmail.com>
2026-02-17 16:43:58 +0100

    Updated Book entity

--- HotelManagementSystem.API/Models/Booking.cs
+++ HotelManagementSystem.API/Models/Booking.cs
@@ -2,5 +2,16 @@
 {
     public class Booking
     {
+        public Guid Id { get; set; }
+
+        public Guid RoomId { get; set; }
+        public Room Room { get; set; }
+
+        public Guid CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
     }
+
 }
