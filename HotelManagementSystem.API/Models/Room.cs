42914081674f7ae0809b4ac394cdd1a6291485fa
Arinzechukwu Ogbu <arinzechukwuwisdomogbu@gmail.com>
2026-02-17 16:08:15 +0100

    Refactor Room entity

--- HotelManagementSystem.API/Models/Room.cs
+++ HotelManagementSystem.API/Models/Room.cs
@@ -1,10 +1,21 @@
-﻿namespace HotelManagementSystem.API.Models
+﻿using HotelManagementSystem.API.Enum;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelManagementSystem.API.Models
 {
     public class Room
     {
+        [Key]
         public Guid Id { get; set; }
         public int RoomNo { get; set; }
-        public string RoomType { get; set; }
-        public bool IsAvalaible { get; set; }
+        public decimal Price { get; set; }
+        public RoomStatus Status { get; set; }
+        public Guid RoomTypeId { get; set; }
+        public RoomType Type { get; set; }       
+        public bool IsAvailable => Status == RoomStatus.Available;
+        public Guid HotelId { get; set; }
+        public Hotel Hotel { get; set; }
+        public List<Booking> Bookings { get; set; }=new List<Booking>();
     }
 }
