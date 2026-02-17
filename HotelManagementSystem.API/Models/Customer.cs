685879979c2be9142a7a1fac9c4ad17d5184872d
Arinzechukwu Ogbu <arinzechukwuwisdomogbu@gmail.com>
2026-02-17 17:14:18 +0100

    Updated Customer entity

--- HotelManagementSystem.API/Models/Customer.cs
+++ HotelManagementSystem.API/Models/Customer.cs
@@ -4,9 +4,12 @@ namespace HotelManagementSystem.API.Models
 {
     public class Customer:BaseEntity
     {
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; } 
         public string Address { get; set; }
+        public DateTime RegisteredAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
         public ICollection<Booking> Bookings { get; set; } = new List<Booking>();
     }
 }
