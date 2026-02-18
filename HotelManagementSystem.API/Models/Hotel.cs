d4112da4abbc5e36edd8573591dd2bf68741b38e
Arinzechukwu Ogbu <arinzechukwuwisdomogbu@gmail.com>
2026-02-18 11:17:16 +0100

    Updated Hotel entity

--- HotelManagementSystem.API/Models/Hotel.cs
+++ HotelManagementSystem.API/Models/Hotel.cs
@@ -1,11 +1,17 @@
 ﻿using HotelManagementSystem.API.Entities;
+using System.Collections;
 
 namespace HotelManagementSystem.API.Models
 {
-    public class Hotel:BaseEntity
+    public class Hotel : BaseEntity
     {
         public string City { get; set; }
         public string Country { get; set; }
-
+        public string Address { get; set; }
+        public decimal Rating { get; set; }
+        public ICollection<Employee> Employees { get; set; } = new List<Employee>();
+        public ICollection<Room> Rooms { get; set; } = new List<Room>();
+        //public Guid RoleId {  get; set; }
+        //public Role Role { get; set; }
     }
 }
